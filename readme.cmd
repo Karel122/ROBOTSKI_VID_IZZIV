5d24796d3354b20c3097c0d1eb3393659bef1642
Karel <145646267+Karel122@users.noreply.github.com>
2025-05-04 18:49:49 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -1,2 +1,6 @@
 Načrt:
-Zadeve se bom lotil \textit{ks}
+Zadeve sem bom lotil tako, da bom uporabil Mediapipe oz. Mediapipe hands. Tako bom iz posnetkov dobil značilnosti posameznih fah in jih shranil. Kasneje bom z uporabo forrest in MLPD naučil prepoznati te značilnosti.
+
+1. Najprej sem ustvaril venv in s pip install nasnel mediapipe in opencv-python.
+2. Uporaben link, ki opisuje mediapipe: https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker
+
