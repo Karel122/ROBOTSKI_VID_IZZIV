8cedb411333d0444c9e0dfed4503f139e8d993ed
Karel <145646267+Karel122@users.noreply.github.com>
2025-05-06 09:20:32 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -4,4 +4,5 @@ Zadeve sem bom lotil tako, da bom uporabil Mediapipe oz. Mediapipe hands. Tako b
 1. Najprej sem ustvaril venv in s pip install nasnel mediapipe in opencv-python.
 2. Uporaben link, ki opisuje mediapipe: https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker
 3. Link, ki opisuje random forrest metodo strojnega učenja: https://github.com/scikit-learn/scikit-learn/tree/main
+4. Še en jupyter notebook primer random forrest-a: https://github.com/JoaquinFeBu/RandomForrestPython/blob/main/Random_Forrest_Fernandez_Joaquin_29648258.ipynb
 
