75e8c1fa49458b8a6284a5a70740eb539a5eaacc
Karel <145646267+Karel122@users.noreply.github.com>
2025-05-30 07:38:12 +0200

    Update readme.cmd

--- readme.cmd
+++ readme.cmd
@@ -1,8 +1,1 @@
-Načrt:
-Zadeve sem bom lotil tako, da bom uporabil Mediapipe oz. Mediapipe hands. Tako bom iz posnetkov dobil značilnosti posameznih fah in jih shranil. Kasneje bom z uporabo forrest in MLPD naučil prepoznati te značilnosti.
-
-1. Najprej sem ustvaril venv in s pip install nasnel mediapipe in opencv-python.
-2. Uporaben link, ki opisuje mediapipe: https://ai.google.dev/edge/mediapipe/solutions/vision/hand_landmarker
-3. Link, ki opisuje random forrest metodo strojnega učenja: https://github.com/scikit-learn/scikit-learn/tree/main
-4. Še en jupyter notebook primer random forrest-a: https://github.com/JoaquinFeBu/RandomForrestPython/blob/main/Random_Forrest_Fernandez_Joaquin_29648258.ipynb
 
